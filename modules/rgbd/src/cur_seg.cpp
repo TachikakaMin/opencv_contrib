f9c54b840389a68df7ef5681397e2e7ac5ab1640
TachikakaMin <tangym@shanghaitech.edu.cn>
2020-08-26 16:12:15 +0000

    algo3

--- modules/rgbd/src/cur_seg.cpp
+++ modules/rgbd/src/cur_seg.cpp
@@ -15,6 +15,7 @@ namespace pcseg
         float len2 = sqrt(v2.x * v2.x + v2.y * v2.y + v2.z * v2.z);
         float dot = v1.dot(v2);
         float a = dot / (len1 * len2);
+
         if (a >= 1.0)
             return 0.0;
         else if (a <= -1.0)
@@ -33,9 +34,12 @@ namespace pcseg
         if (channel < 6)
         {
             Mat pointsWithNormal2;
-            ppf_match_3d::computeNormalsPC3d(pointsWithNormal, pointsWithNormal2, k, 0, Vec3f(0,0,0));
+            int x = ppf_match_3d::computeNormalsPC3d(pointsWithNormal, pointsWithNormal2, k, 0, Vec3f(0,0,0));
+            printf("computeNormalsPC3d: %d\n",x);
             pointsWithNormal = pointsWithNormal2;
+            // ppf_match_3d::writePLY(pointsWithNormal, "pickup_big_normal.ply");
             channel = pointsWithNormal.size().width;
+            printf("channel: %d\n",channel);
         }
         for (int i=0;i<len;i++)
         {
@@ -70,22 +74,31 @@ namespace pcseg
             float a = pca.eigenvalues.at<float>(0);
             float b = pca.eigenvalues.at<float>(size/2);
             float c = pca.eigenvalues.at<float>(size-1);
+            // std::cout<<c/(a+b+c)<<std::endl;
             curvatures.push_back(c/(a+b+c));
         }
         return curvatures;
     }
 
+    int findFather(std::vector<int>& v, int x)
+    {
+        if (v[x] != x) v[x] = findFather(v[x]);
+        return x;
+    }
 
-    std::vector<int> planarSegments(
+    bool planarSegments(
             Mat& pointsWithNormal,
             std::vector<float>& curvatures,
             int k,
             float thetaThreshold,
-            float curvatureThreshold)
+            float curvatureThreshold,
+            std::vector<std::vector<Point3f> >& vecRetPoints,
+            std::vector<std::vector<Point3f> >& vecRetNormals
+    )
     {
 
         std::vector<Point3f> points;
-        std::vector<Point3f> normal;
+        std::vector<Point3f> normals;
         int len = pointsWithNormal.size().height;
         int channel = pointsWithNormal.size().width;
         if (channel != 6) CV_Error(Error::StsBadArg, String("No Normal Channel!"));
@@ -94,7 +107,7 @@ namespace pcseg
             Point3f p(pointsWithNormal.at<float>(i,0), pointsWithNormal.at<float>(i,1), pointsWithNormal.at<float>(i,2));
             points.push_back(p);
             Point3f q(pointsWithNormal.at<float>(i,3), pointsWithNormal.at<float>(i,4), pointsWithNormal.at<float>(i,5));
-            normal.push_back(q);
+            normals.push_back(q);
         }
 
 
@@ -106,7 +119,7 @@ namespace pcseg
         std::queue<int> q;
         std::vector<bool> isSeg(len, 0 );
         std::vector<int> idSeg(len);
-        for (int i=0;i<len;i++) idSeg[i] = i;
+        for (int i=0;i<len;i++) idSeg[i] = i; // disjoint set as index counter
 
         while (isSegCount < len || !q.empty()) {
             int seedPointId = -1;
@@ -132,7 +145,7 @@ namespace pcseg
             kdtree.knnSearch(query, indices, dists, k);
             for (int i=0;i<indices.size();i++)
             {
-                if (angleBetween(normal[seedPointId], normal[indices[i]]) < thetaThreshold)
+                if (angleBetween(normals[seedPointId], normals[indices[i]]) < thetaThreshold)
                 {
                     idSeg[indices[i]] = idSeg[seedPointId];
                     if (curvatures[indices[i]] < curvatureThreshold && isSeg[indices[i]] == 0)
@@ -144,42 +157,63 @@ namespace pcseg
                 }
             }
         }
-        return idSeg;
+
+        std::map<int, std::vector<Point3f> > retPoints;
+        std::map<int, std::vector<Point3f> > retNormals;
+        for (int i=0;i<idSeg.size();i++)
+        {
+            int idx = findFather(i); // disjoint set
+            if (retPoints.find(idx) == retPoints.end())
+            {
+                std::vector<int> tmp;
+                retPoints[idx] = tmp;
+                std::vector<int> tmp2;
+                retNormals[idx] = tmp2;
+                // first point is plane center point and normal is plane normal
+                retPoints[idx].push_back(points[idx]);
+                retNormals[idx].push_back(normals[idx]);
+            }
+            retPoints[idx].push_back(points[i]);
+            retNormals[idx].push_back(normals[i]);
+        }
+
+        // first is Multi-Planar Points, second is Multi-Planar Normals
+        std::map<int, std::vector<Point3f> >::iterator it;
+        for ( it = retPoints.begin(); it != retPoints.end(); it++ )
+        {
+            int i = it->first;
+            vecRetPoints.push_back[retPoints[i]];
+            vecRetNormals.push_back[retNormals[i]];
+        }
+        return true;
     }
 
-    bool planarMerge(Mat& pointsWithNormal ,
-                     std::vector<int>& idA,
-                     Point3f& normalA,
-                     int idACenter,
-                     double& timestepA,
-                     std::vector<int>& idB,
-                     Point3f& normalB,
-                     int idBCenter,
-                     float disThreshold)
+    bool planarMerge(
+            std::vector<Point3f>& pointsA,
+            std::vector<Point3f>& normalsA,
+            float& timestepA,
+            std::vector<Point3f> pointsB,
+            std::vector<Point3f> normalsB,
+            float disThreshold)
     {
-        std::vector<Point3f> points;
-        std::vector<Point3f> normal;
-        int len = pointsWithNormal.size().height;
-        int channel = pointsWithNormal.size().width;
-        if (channel != 6) CV_Error(Error::StsBadArg, String("No Normal Channel!"));
-        for (int i=0;i<len;i++)
+        Point3f aCenter = pointsA[0];
+        Point3f bCenter = pointsB[0];
+        Point3f& normalA = normalsA[0];
+        Point3f& normalB = normalsB[0];
+        std::vector<int> indicesConcaveB;
+        indicesConcaveB =
+        for (int i=0;i<indicesConcaveB.size();i++)
         {
-            Point3f p(pointsWithNormal.at<float>(i,0), pointsWithNormal.at<float>(i,1), pointsWithNormal.at<float>(i,2));
-            points.push_back(p);
-            Point3f q(pointsWithNormal.at<float>(i,3), pointsWithNormal.at<float>(i,4), pointsWithNormal.at<float>(i,5));
-            normal.push_back(q);
-        }
-        Point3f aCenter = points[idACenter];
-        Point3f bCenter = points[idBCenter];
-        for (int i=0;i<idB.size();i++)
-        {
-            Point3f h = points[idB[i]];
+            Point3f h = pointsB[indicesConcaveB[i]];
             Point3f dis = aCenter - h;
             if (dis.dot(dis) < disThreshold*disThreshold)
             {
                 normalA = normalA*sqrt(aCenter.dot(aCenter)) + normalB*sqrt(bCenter.dot(bCenter));
                 normalA /= (sqrt(aCenter.dot(aCenter)) + sqrt(bCenter.dot(bCenter)));
-                for (int j=0;j<idB.size();j++) idA.push_back(idB[j]);
+                for (int j=1;j<pointsB.size();j++)
+                {
+                    idA.push_back(idB[j]);
+                }
                 timestepA = 0;
                 idB.clear();
                 return true;
@@ -198,7 +232,7 @@ namespace pcseg
                        std::vector<Point3f>& normalQs,
                        std::vector<int>& idCenterQs,
                        std::vector<float>& timestepQs,
-                       Point6f& curCameraPos,
+                       Point3f& curCameraPos,
                        float thetaThreshold,
                        float timestepThreshold,
                        float timestepDisThreshold
@@ -206,17 +240,55 @@ namespace pcseg
     {
         for (int i=0;i<idNs.size();i++)
         {
+            std::vector<int> R;
+            R.clear();
             bool gotPlane = 0;
             for (int j=0;j<idQs.size();j++)
             {
-                if (!finalQ[i] && angleBetween(normalNs[i], normalQs[i]) < thetaThreshold)
+                if (!finalQ[i] && angleBetween(normalNs[i], normalQs[j]) < thetaThreshold)
                 {
                     std::vector<int> indicesConcave;
                     indicesConcave = getConcaveHull(newPointsWithNormal, idNs[i]);
-                    gotPlane = planarMerge()
+                    gotPlane = planarMerge();
+                    if (gotPlane) break;
+                    else R.push_back(j);
+                }
+            }
+
+            if (!gotPlane)
+            {
+                kdtree N ic;
+                timestepNs[i] = 0;
+                idQs.push_back(idNs[i]);
+                normalQs.push_back(normalNs[i]);
+                idCenterQs.push_back(idCenterNs[i]);
+                timestepQs.push_back(timestepNs[i]);
+
+
+
+            }
+            removeNicFromM;
+        }
+
+        for (int i=0;i<idQs.size();i++)
+        {
+            timestepQs[i] ++;
+            if (timestepQs[i] > timestepThreshold)
+            {
+                finalQ[i] = 1;
+                std::vector<int> ind = idQs[i];
+                for (int j=0;j<ind.size();j++)
+                {
+                    Point3f p = curCameraPos - points[ind[j]];
+                    if (p.dot(p) < timestepDisThreshold)
+                    {
+                        finalQ[i] = 0;
+                        break;
+                    }
                 }
             }
         }
     }
+
 }
 }
--- modules/rgbd/src/cur_seg.hpp
+++ modules/rgbd/src/cur_seg.hpp
@@ -20,21 +20,23 @@ namespace pcseg
 {
     float angleBetween(const Point3f& , const Point3f&);
     std::vector<float> calCurvatures(Mat& , int);
-    std::vector<int> planarSegments(
-            Mat&,
-            std::vector<float>&,
-            int,
+    int findFather(std::vector<int>& , int );
+    bool planarSegments(
+            Mat& ,
+            std::vector<float>& ,
+            int ,
             float ,
+            float ,
+            std::vector<std::vector<Point3f> >& ,
+            std::vector<std::vector<Point3f> >&
+    );
+    bool planarMerge(
+            std::vector<Point3f>& ,
+            std::vector<Point3f>& ,
+            float& ,
+            std::vector<Point3f>& ,
+            std::vector<Point3f>& ,
             float );
-    bool planarMerge(Mat&  ,
-                     std::vector<int>& ,
-                     Point3f& ,
-                     int ,
-                     double& ,
-                     std::vector<int>& ,
-                     Point3f& ,
-                     int ,
-                     float );
     void growingPlanar(Mat& ,
                        std::vector<std::vector<int> >& ,
                        std::vector<Point3f>& ,
